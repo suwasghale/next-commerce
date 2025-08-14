b67d4749bc13eccf5c18585052c40a2818e7d02b
SuwasG <suwasghale693@gmail.com>
2025-08-14 21:55:41 +0545

    unsplash domain images to nextcofig set

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+module.exports = nextConfig;
 
-export default nextConfig;
