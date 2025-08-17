ad4b3426af510045ac8b840911b638ccda744347
SuwasG <suwasghale693@gmail.com>
2025-08-17 17:40:13 +0545

    cart route created

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import CartSummary from "@/components/cart/CartSummary";
+
+export default function CartPage() {
+  return (
+    <div className="max-w-4xl mx-auto my-8">
+      <CartSummary />
+    </div>
+  );
+}
