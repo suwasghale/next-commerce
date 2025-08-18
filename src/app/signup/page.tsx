ac109ce91bee21e7f6f91952623b4cced1948692
SuwasG <suwasghale693@gmail.com>
2025-08-18 15:46:08 +0545

    signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {Signup} from "@/components/auth/Signup";
+
+
+const SignPage: React.FC = () => {
+
+  return (
+    <>
+    <div className="flex justify-center py-10">
+               <Signup/> 
+    </div>
+      
+    </>
+  );
+};
+
+export default SignPage;
