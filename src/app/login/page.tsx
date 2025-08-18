1d987bee87650fe62556e313e6f45adcdf73d345
SuwasG <suwasghale693@gmail.com>
2025-08-18 15:45:48 +0545

    login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import {Login} from "@/components/auth/Login";
+
+
+const LoginPage: React.FC = () => {
+
+  return (
+    <>
+    <div className="flex justify-center py-10">
+               <Login/> 
+    </div>
+      
+    </>
+  );
+};
+
+export default LoginPage;
